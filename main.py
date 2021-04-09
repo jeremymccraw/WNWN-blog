a43f0daa3c5ec2ab7eaca616b9c1b9593816a123
Jeremy <jrmccraw@icloud.com>
2021-04-09 19:58:51 +0200

    Add gunicorn

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
